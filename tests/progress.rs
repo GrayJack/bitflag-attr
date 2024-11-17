f6ba1899122d8f5061ea1cdacab50924ffaf850c
GrayJack <gr41.j4ck@gmail.com>
2024-11-16 21:15:53 -0300

    feat: Improve error message on parameters being repeated

--- tests/progress.rs
+++ tests/progress.rs
@@ -5,4 +5,5 @@ fn tests() {
     t.compile_fail("tests/01-invalid_type");
     t.compile_fail("tests/02-no_value");
     t.compile_fail("tests/03-too_many_args");
+    t.compile_fail("tests/04-repetitive_args");
 }
