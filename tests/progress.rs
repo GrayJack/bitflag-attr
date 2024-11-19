4b2ee8a9efb2006d6c62930f76c6d30dab88f7bc
GrayJack <gr41.j4ck@gmail.com>
2024-11-19 00:34:58 -0300

    tests: Add test for no_std

--- tests/progress.rs
+++ tests/progress.rs
@@ -6,4 +6,5 @@ fn tests() {
     t.compile_fail("tests/02-no_value");
     t.compile_fail("tests/03-too_many_args");
     t.compile_fail("tests/04-repetitive_args");
+    t.pass("tests/05-no_std");
 }
